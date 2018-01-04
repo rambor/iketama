34fee007137df19dd28b061dd9e52306cc227626
rambor <robert_p_rambo@diamond.ac.uk>
2018-01-04 12:33:32 +0000

    Removed dangling pointer references in destructor

--- src/EulerTour/EulerTour.h
+++ src/EulerTour/EulerTour.h
@@ -13,7 +13,7 @@
 #include <memory>
 #include <unordered_set>
 #include "Node.h"
-#include "SimpleTour.h"
+//#include "SimpleTour.h"
 #include <ctime>
 
 class Model;
@@ -26,8 +26,8 @@ class Model;
  */
 
 
-struct find_by_key : std::unary_function< Node *, bool>{
-    find_by_key(int keyToFind) : key(keyToFind){}
+struct find_Node_by_key : std::unary_function< Node *, bool>{
+    find_Node_by_key(int keyToFind) : key(keyToFind){}
     bool operator () (Node * p) { return p->getKey() == key; }
 private:
     int key;
@@ -36,27 +36,20 @@ private:
 
 class EulerTour {
 
-    typedef std::map<int,Node>::iterator it_type;
+    typedef std::map<int, Node>::iterator it_type;
 
     int totalComponents;
     std::map<int, Node> nodes; // use shared pointer? make instance on heap
-    //std::map<int, std::shared_ptr<Node> > nodes;
-    //std::vector<int>::iterator * pSelectedLattice;
-
-    std::map<int, SimpleTour> simpleTours;
+    //std::map<int, SimpleTour> simpleTours;
     std::map<int, std::list< Node *> > tours; // key is the root of the tour
-    // std::map<int, Tours> => tours is an object that contains List and Set
 
     void createInitialTour(int workingLimit, Model *pModel, std::vector<int>::iterator beginIt);
     bool addToTour(int nodeToAdd);
-    bool addToSimpleTour(int nodeToAdd);
+    //bool addToSimpleTour(int nodeToAdd);
 
     void createSubTour(Node * pNode, std::list< Node * > * subTourToLoad);
     void rerootSubTour(Node * newRoot, std::list< Node * > * subTourToLoad);
     void rerootAndReassignSubTour(Node * newRoot, std::list< Node * > * subTourToLoad);
-
-    std::list< Node * >::iterator getFirstOccurrenceInTour(Node * pNode, std::list<Node *> * pTour);
-    std::list< Node * >::reverse_iterator getLastOccurrenceInTour(Node * pNode, std::list<Node *> * pTour);
 
     void testSetOne();
     void printList(std::string text, std::list< Node * > * list);
@@ -67,37 +60,41 @@ class EulerTour {
     bool validateTour(std::list<Node *> * tourtocheck);
     void removeListFromTour(int key);
 
+    void printTourSizes();
+
 public:
 
     // pass in iterator to beginning of selected lattice points that are sorted upto WorkingLimit
     // want to use this Class to determine connectivity of a graph
     // Connectivity is the numberOfComponents
     //
-    EulerTour(std::vector<int>::iterator beginIt, int workingLimit, Model *pModel);
     EulerTour();
+    EulerTour(std::vector<int>::iterator beginIt, int workingLimit, Model *pModel);
+    EulerTour(std::set<int> & beginIt, Model *pModel);
+
     ~EulerTour(){
 
         // clear pointers to nodes
-        for (std::map<int, std::list< Node *>>::iterator it=tours.begin(); it!=tours.end(); ++it){
-            // delete points in tour
+//        for (std::map<int, std::list< Node *>>::iterator it=tours.begin(); it!=tours.end(); ++it){
+//            // delete points in tour
 //            for(std::list<Node *>::iterator lit = it->second.begin(); lit != it->second.end(); ++lit){
 //                it->second.erase(lit);
 //            }
-            it->second.clear();
-        }
-        // remove all nodes
-        std::map<int,Node>::iterator it = nodes.begin();
-
-        while(it != nodes.end()){
-            it = nodes.erase(it);
-        }
-
-        tours.clear();
-        nodes.clear();
+//            it->second.clear();
+//        }
+//        // remove all nodes
+//        std::map<int,Node>::iterator it = nodes.begin();
+//        while(it != nodes.end()){
+//            it = nodes.erase(it);
+//        }
+//
+//        tours.clear();
+//        nodes.clear();
     }
 
     int addNode(int latticePoint, Model *pModel);
     int removeNode(int indexOfNode);
+
     int removeNodeFromSimpleTour(int indexOfNode);
     void rerootActiveTour(Node * newRoot);
 
@@ -110,7 +107,7 @@ public:
     void checkTourSize(std::string note);
     bool validateList(std::string note);
     bool checkNodesList(std::set<int> * beads_in_use);
-    static bool deleteAll( Node * theElement ) { delete theElement; return true; }
+
 };
 
 #endif //IKETAMA_EULERTOUR_H
